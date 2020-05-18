57ccc030a5cd16b52e253042406419d8dd1dde1b
Matthew Mattox <matthew.mattox@rancher.com>
2020-05-18 10:03:09 -0500

    Making sure docker service starts'

--- install.sh
+++ install.sh
@@ -7,4 +7,7 @@ if ! docker ps
 then
   echo "Need to install docker..."
   curl https://raw.githubusercontent.com/rancher/install-docker/master/19.03.8.sh | bash
+  systemctl enable docker
+  systemctl start docker
 fi
+
