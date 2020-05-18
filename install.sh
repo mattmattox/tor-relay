f76eb0a31f8311df7eb567a01050e60085bc8cb0
Matthew Mattox <matthew.mattox@rancher.com>
2020-05-18 10:00:04 -0500

    Fixing typo

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 apt update -y
 apt upgrade -y
 
-if docker ps
+if ! docker ps
 then
   echo "Need to install docker..."
   curl https://raw.githubusercontent.com/rancher/install-docker/master/19.03.8.sh | bash
